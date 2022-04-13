8e556bdf6860f4848ad0f89c96dba6067b9d6b89
iysheng <iysheng@163.com>
2022-04-13 18:51:14 +0800

    [ADD][N/A] 添加获取 cscope 的脚本

---
+++ board/GD32F310G_START/GCC/hello_world/gencscope.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TOPDIR=/home/red/Projects/TencentOS-tiny
+BOARDDIR=$TOPDIR/board/GD32F310G_START
+KERNELDIR=$TOPDIR/kernel
+OSALDIR=$TOPDIR/osal
+ARCHDIR=$TOPDIR/arch/arm/arm-v7m/common
+ARCHDIR1=$TOPDIR/arch/arm/arm-v7m/cortex-m4
+VENORDIR=$TOPDIR/platform/vendor_bsp/gd/GD32F3x0_Firmware_Library
+
+
+fd -e c -e h -e s -e S . $BOARDDIR > cscope.files
+fd -e c -e h -e s -e S . $KERNELDIR >> cscope.files
+fd -e c -e h -e s -e S . $OSALDIR >> cscope.files
+fd -e c -e h -e s -e S . $ARCHDIR >> cscope.files
+fd -e c -e h -e s -e S . $ARCHDIR1 >> cscope.files
+fd -e c -e h -e s -e S . $VENORDIR >> cscope.files
+
+cscope -bqR
