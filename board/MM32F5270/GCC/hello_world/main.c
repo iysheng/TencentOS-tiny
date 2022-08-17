e7f4cb4e9337d8ec0194035d14a2e0111c874b1b
iysheng <iysheng@163.com>
2022-08-17 17:56:40 +0800

    [CHG][N/T] 保证编译通过，关联 tos

--- board/MM32F5270/GCC/hello_world/main.c
+++ board/MM32F5270/GCC/hello_world/main.c
@@ -6,35 +6,48 @@
  */
 
 #include "board_init.h"
+#include "tos_shell.h"
 
-/*
- * Macros.
- */
+#define APPLICATION_TASK_STK_SIZE       0x100
+k_task_t application_task;
+uint8_t application_task_stk[APPLICATION_TASK_STK_SIZE];
+static char gs_cmd_buffer[32];
 
-/*
- * Variables.
- */
+__attribute__ ((__weak__)) void application_entry(void *arg)
+{
+    while (1) {
+        tos_task_delay(1000);
+    }
+}
 
-/*
- * Declerations.
- */
+static void board_shell_output_t(const char ch)
+{
+    if (ch == '\n')
+    {
+        while ( 0u == (UART_STATUS_TX_EMPTY & UART_GetStatus(BOARD_DEBUG_UART_PORT)) )
+        {}
+        UART_PutData(BOARD_DEBUG_UART_PORT, '\n');
+    }
+    while ( 0u == (UART_STATUS_TX_EMPTY & UART_GetStatus(BOARD_DEBUG_UART_PORT)) )
+    {}
+    UART_PutData(BOARD_DEBUG_UART_PORT, ch);
+}
 
 /*
  * Functions.
  */
 int main(void)
 {
-    uint8_t ch;
-
     BOARD_Init();
 
     printf("hello, world\r\n");
 
-    while (1)
-    {
-        ch = getchar();
-        putchar(ch);
-    }
+    tos_knl_init();
+    tos_task_create(&application_task, "application_task", application_entry, NULL, 4, application_task_stk, APPLICATION_TASK_STK_SIZE, 0);
+    tos_shell_init(gs_cmd_buffer, sizeof(gs_cmd_buffer), board_shell_output_t, NULL);
+    tos_knl_start();
+
+    return 0;
 }
 
 /* EOF. */
